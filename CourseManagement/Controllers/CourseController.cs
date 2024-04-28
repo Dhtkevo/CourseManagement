10a2425f20cf80dea5c0a582ce029e908c6f6dc9
km00372 <km00372@CSVLAB-VM10>
2024-04-28 11:56:49 -0400

    Create course model class

---
+++ CourseManagement/Controllers/CourseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace CourseManagement.Controllers
+{
+    public class CourseController : Controller
+    {
+       public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CourseManagement/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace CourseManagement.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Subject { get; set; }
+       
+        public string Description { get; set; }
+    }
+}
