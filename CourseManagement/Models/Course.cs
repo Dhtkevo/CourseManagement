48cc6da869d61d9dac3972aea4004ff16f5e35b8
km00372 <km00372@CSVLAB-VM10>
2024-04-30 09:29:12 -0400

    Update display details about courses and add validation to fields

--- CourseManagement/Models/Course.cs
+++ CourseManagement/Models/Course.cs
@@ -6,8 +6,12 @@ namespace CourseManagement.Models
     {
         public int Id { get; set; }
         [Display(Name = "Course Name")]
+        [StringLength(30, MinimumLength = 10)]
+        [Required]
         public string? Name { get; set; }
 
+        [StringLength(20, MinimumLength = 4)]
+        [Required]
         public string? Subject { get; set; }
        
         public string? Description { get; set; }
